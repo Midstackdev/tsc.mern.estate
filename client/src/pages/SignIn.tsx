1ce9a53f151a61cc66ab4f0eaf9af0393d00be4c
Midstackdev <midstackdev@gmail.com>
2024-04-12 17:39:27 +0000

    complete signin

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,27 +1,47 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { InputChangeEventHandler } from '../types';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppError, FormEvent, InputChangeEventHandler } from '../types';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | AppError>(null);
+  const navigate = useNavigate();
+
   const handleChange = (e: InputChangeEventHandler) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-  console.log(formData);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) throw data;
+      console.log(data);
+      setLoading(false);
+      setError(null);
+      navigate('/profile');
+    } catch (err) {
+      setError(err as AppError);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="Fullname"
-          className="border p-3 rounded-lg focus:outline-none"
-          id="name"
-          onChange={handleChange}
-        />
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="Email"
@@ -37,11 +57,11 @@ const SignIn = () => {
           onChange={handleChange}
         />
         <button
-          type="button"
-          // disabled
+          type="submit"
+          disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          Sign in
+          {loading ? 'loading...' : 'Sign in'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -50,6 +70,7 @@ const SignIn = () => {
           <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error.message}</p>}
     </div>
   );
 };
