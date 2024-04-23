c1512d9f1641c2c5c0fc8fe8e472be3165ba85ac
Midstackdev <midstackdev@gmail.com>
2024-04-23 09:48:55 +0000

    listing routes

--- server/src/app/Http/Controllers/Listing.ts
+++ server/src/app/Http/Controllers/Listing.ts
@@ -1,20 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import Controller from "../../libs/routing/Controller";
 import { Listing } from "../../Models/Listing";
+import { throwNotFoundError } from "../../libs/utils/errors";
 
 export class ListingController extends Controller {
   public constructor() {
     super();
   }
 
-  public async index(req: Request, res: Response) {
-    super.jsonRes(
-      {
-        message: "REST API HOME ROUTE IS HEALTHY",
-        type: "success",
-      },
-      res
-    );
+  public async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      //validate admin
+      const listings = await Listing.findMany({});
+      return super.jsonRes(listings, res);
+    } catch (error) {
+      next(error);
+    }
   }
 
   public async store(req: Request, res: Response, next: NextFunction) {
@@ -22,6 +23,19 @@ export class ListingController extends Controller {
       //validate fields
       const listing = await Listing.create(req.body);
       return super.jsonRes(listing, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async userListings(req: Request, res: Response, next: NextFunction) {
+    try {
+      //validate authorized user
+      if (req.user.id !== req.params.id) {
+        throwNotFoundError("User not found");
+      }
+      const listings = await Listing.findMany({ userRef: req.user.id });
+      return super.jsonRes(listings, res);
     } catch (error) {
       next(error);
     }
--- server/src/app/routes/listing.ts
+++ server/src/app/routes/listing.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { controller } from "../Http/Controllers/Listing";
+import { protect } from "../Http/Middleware/protected";
 
 const router = Router();
 
 router.get("/", controller.index);
 router.post("/", controller.store);
+router.get("/user/:id", protect, controller.userListings);
 
 export default router;
