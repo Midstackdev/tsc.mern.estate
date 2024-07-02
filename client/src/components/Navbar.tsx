a245b743f18cc7cc14e4a9c7d16c7c27a815a782
Midstackdev <midstackdev@gmail.com>
2024-07-02 14:37:53 +0000

    url search

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { SearchIcon } from '../assets/icons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 
 const Navbar = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('term', searchTerm);
+    const query = urlParams.toString();
+
+    navigate(`/search?${query}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const search = urlParams.get('term');
+
+    if (search) {
+      setSearchTerm(search);
+    }
+  }, []);
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -21,8 +40,12 @@ const Navbar = () => {
             type="text"
             placeholder="Search...."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <SearchIcon className="text-slate-600 w-4 h-4 cursor-pointer" />
+          <button type="button" onClick={handleSearch}>
+            <SearchIcon className="text-slate-600 w-4 h-4 cursor-pointer" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to={'/'}>
